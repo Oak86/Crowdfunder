6fabeb1261fa7d64733bff620ed18e216a6b530d
oak86 <email@omarkhafagy.com>
2013-09-30 13:45:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdfunder::Application.config.secret_token = '18707ced923e187d6df6834781fbb0f6a71388a94f43993d937e8ea5f4bd1d681105ea871c9c1c1f955cbf09c4d97a10293c39d566fa71a384b352c94ca79494'
