043bf40e02d5aa62c424b5ffdd73f8e98578b877
oak86 <email@omarkhafagy.com>
2013-10-02 13:04:49 -0400

    fix test and delete requests/project_flows_spec

--- spec/features/project_flows_spec.rb
+++ spec/features/project_flows_spec.rb
@@ -15,17 +15,11 @@ describe "Project List" do
 			expect(current_path).to eq(projects_path)
 
 			#Expect that this page has the words 'Listing Projects'
-			page.should have_content('Listing projects')
 			expect(page).to have_content('Listing projects')
 
 			#Expect this page has these words as well
-			page.should have_content('Project 1')
 			expect(page).to have_content('Project 1')
-
-			page.should have_content('Project 2')
 			expect(page).to have_content('Project 2')
-
-			page.should have_content('Project 3')
 			expect(page).to have_content('Project 3')
 		end
 	end
