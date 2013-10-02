a97373cc6c7ceb4abb45af40f674234c29fa7aaa
oak86 <email@omarkhafagy.com>
2013-10-02 14:26:00 -0400

    Writing tests for proejcts show page

--- spec/features/project_flows_spec.rb
+++ spec/features/project_flows_spec.rb
@@ -21,6 +21,14 @@ describe "Project List" do
 			expect(page).to have_content('Project 1')
 			expect(page).to have_content('Project 2')
 			expect(page).to have_content('Project 3')
+
+			#Click a link to the project 1's show page
+			click_link('Project 1')
+			#Expect we're on project1's show page
+			expect(current_path).to eq(project_path(project1))
+			#Expect on this page the first h1 has the text project1's title
+			page.should have_selector('h1:first', text: project1.title)
+			expect(page).to have_selector('h1:first', text: project1.title)
 		end
 
 		it "should display the navigation" do
